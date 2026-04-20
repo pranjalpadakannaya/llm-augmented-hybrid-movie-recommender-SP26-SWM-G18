f11ecf8e4d0f5800d6bef522c4f9778e52a03ba8
Sanjay <sanjayso@asu.edu>
2026-04-19 18:35:37 -0700

    fix test lint error

--- backend/tests/test_smoke.py
+++ backend/tests/test_smoke.py
@@ -1,6 +1,8 @@
 def test_imports():
-    import backend.models.kg
-    import backend.models.gru4rec
-    import backend.models.occf
+    import backend.models.kg as kg
+    import backend.models.gru4rec as gru4rec
+    import backend.models.occf as occf
 
-    assert True
+    assert kg is not None
+    assert gru4rec is not None
+    assert occf is not None
